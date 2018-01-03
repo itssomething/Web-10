663c074fe78417ca29c8b18e6c29126aae252286
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-01-03 20:26:06 +0700

    Hihi

--- SaaS/app.js
+++ SaaS/app.js
@@ -1,32 +1,32 @@
-//Require thư viện
 const express = require('express');
-const app = express();
 const config = require('./config.json');
-//Require module tự tạo
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
 const fileController = require('./fileController');
+const askRouter = require('./askRouter');
 
-console.log(fileController.readFileSync('testFileController.txt'));
+let app = express();
 
-//Đặt folder public là folder static của server
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
+
+app.get('/', (req, res)=>{
+    res.render("index", {
+        layout: "other",
+        html: "<h1>Hello</h1>"
+    });
+});
+
+app.get('/about', (req, res)=>{
+    
+});
+
+app.use('/ask', askRouter);
+
 app.use(express.static('public'));
 
-//Xử lý respone trả về khi người dùng request đến đường dẫn /
-app.get('/', (req, res)=>{
-    console.log(__dirname)
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-//Xử lý respone trả về khi người dùng request đến đường dẫn /menu
-app.get('/menu', (req, res)=>{
-    res.sendFile(__dirname + "/public/menu_2.html");
-});
-
-//Xử lý respone trả về khi người dùng request đến đường dẫn /about
-app.get('/about', (req, res)=>{
-    res.send("Hi! I'm Huy hihi");
-});
-
-//Start server ở địa chỉ http://localhost:config.port với config.port thiết lập trong file config.json
 app.listen(config.port, (err) => {
     if (err) { console.log(err); };
     console.log(`App is listening at port ${config.port}`);
---
+++ SaaS/askRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Router = express.Router();
+
+Router.get('/', (req, res)=>{
+    res.render("ask");
+});
+
+Router.get('/sub', (req, res)=>{
+    res.send({
+        a: 4,
+        b: 5
+    });
+});
+
+Router.post('/', (req, res)=>{
+});
+
+module.exports = Router;
